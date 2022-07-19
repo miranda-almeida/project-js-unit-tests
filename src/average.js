2f7dca1df987cc96c5f7c88eb2a3bb6b5170711c
miranda <magui@duck.com>
2022-07-19 15:27:11 -0300

    requisito 1: função average

--- src/average.js
+++ src/average.js
@@ -11,8 +11,18 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {
-  
+const average = (array) => {
+  let sum = 0;
+  for (let index = 0; index < array.length; index += 1) {
+    if (typeof array[index] !== 'number') {
+      return undefined;
+    }
+    sum += array[index];
+  }
+  if (array.length === 0) {
+    return undefined;
+  }
+  return Math.round(sum / array.length);
 };
 
 module.exports = average;
